3cd1f3ee14bc06f1136d9bbc00bd9dff1b2ec8e9
Siddharth9616 <siddharthrai251@gmail.com>
2025-12-24 22:24:45 +0530

    Auto create admin for Render

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -12,6 +12,21 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
+if os.environ.get("RENDER") == "true":
+    try:
+        from django.contrib.auth.models import User
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="Siddharth",
+                email="siddharthrai251@gmail.com",
+                password="Amanrai9616"
+            )
+    except Exception:
+        pass
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
