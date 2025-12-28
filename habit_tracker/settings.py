58a19ea30ecd46a7499c91c7661daa56c561194a
Siddharth9616 <siddharthrai251@gmail.com>
2025-12-28 19:25:56 +0530

    Fix profile page production error

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -71,10 +71,11 @@ ROOT_URLCONF = 'habit_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
